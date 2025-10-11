09cdef76726e396f635807779520446773b00df6
Vaxry <vaxry@vaxry.net>
2025-10-11 20:30:09 +0100

    core: add a protocol scanner

--- tests/Client.cpp
+++ tests/Client.cpp
@@ -1,62 +1,30 @@
 #include <hyprwire/hyprwire.hpp>
-#include "Spec.hpp"
 #include <print>
+#include "generated/test_protocol_v1-client.hpp"
 
 using namespace Hyprutils::Memory;
 
 #define SP CSharedPointer
 
-static SP<CTestProtocolSpec>       spec = makeShared<CTestProtocolSpec>();
-static SP<Hyprwire::IObject>       obj;
+constexpr const uint32_t           TEST_PROTOCOL_VERSION = 1;
+
+static SP<CCTestProtocolV1Impl>    impl = makeShared<CCTestProtocolV1Impl>(TEST_PROTOCOL_VERSION);
+static SP<CCMyManagerV1Object>     manager;
+static SP<CCMyObjectV1Object>      object;
 static SP<Hyprwire::IClientSocket> sock;
 
-//
-static void onObjectS2CMessage(Hyprwire::IObject* obj, const char* data) {
-    std::println("Received: {}", data);
-    if (data == std::string{"You bound!"}) {
-        obj->call(0, "Sure did! Over VaxWire™");
-        obj->call(0, "I am now binding a new object!");
-        auto id  = obj->call(1);
-        auto obj = sock->objectForId(id);
-
-        obj->call(0, std::format("Hey, seems like I got id {}!", id).c_str());
-    }
-}
-
-class CTestProtocolImpl : public Hyprwire::IProtocolClientImplementation {
-  public:
-    virtual ~CTestProtocolImpl() = default;
-
-    virtual SP<Hyprwire::IProtocolSpec> protocol() {
-        return spec;
-    }
-
-    virtual std::vector<SP<Hyprwire::SClientObjectImplementation>> implementation() {
-        return {
-            makeShared<Hyprwire::SClientObjectImplementation>(Hyprwire::SClientObjectImplementation{
-                .objectName = "my_manager",
-                .version    = 1,
-            }),
-            makeShared<Hyprwire::SClientObjectImplementation>(Hyprwire::SClientObjectImplementation{
-                .objectName = "my_object",
-                .version    = 1,
-            }),
-        };
-    }
-};
-
-int main(int argc, char** argv, char** envp) {
+int                                main(int argc, char** argv, char** envp) {
     const auto XDG_RUNTIME_DIR = getenv("XDG_RUNTIME_DIR");
     sock                       = Hyprwire::IClientSocket::open(XDG_RUNTIME_DIR + std::string{"/test-hw.sock"});
 
-    sock->addImplementation(makeShared<CTestProtocolImpl>());
+    sock->addImplementation(impl);
 
     if (!sock->waitForHandshake()) {
         std::println("err: handshake failed");
         return 1;
     }
 
-    const auto SPEC = sock->getSpec("my_protocol");
+    const auto SPEC = sock->getSpec(impl->protocol()->specName());
 
     if (!SPEC) {
         std::println("err: test protocol unsupported");
@@ -65,12 +33,15 @@ int main(int argc, char** argv, char** envp) {
 
     std::println("test protocol supported at version {}. Binding.", SPEC->specVer());
 
-    obj = sock->bindProtocol(spec, 1);
+    manager = makeShared<CCMyManagerV1Object>(sock->bindProtocol(impl->protocol(), TEST_PROTOCOL_VERSION));
 
     std::println("Bound!");
 
-    obj->call(0, "Hello!");
-    obj->listen(0, rc<void*>(::onObjectS2CMessage));
+    manager->sendSendMessage("Hello!");
+    manager->setSendMessage([](const char* msg) { std::println("Server says {}", msg); });
+    object = makeShared<CCMyObjectV1Object>(manager->sendMakeObject());
+    object->setSendMessage([](const char* msg) { std::println("Server says on object {}", msg); });
+    object->sendSendMessage("Hello on object");
 
     std::println("Sent hello!");
 
