a1265ae4ce85584791942370e68b9e906a8bb0b7
Hiroki Tagato <tagattie@gmail.com>
2026-04-28 20:29:12 +0900

    Add missing include

--- tests/Fork.cpp
+++ tests/Fork.cpp
@@ -3,6 +3,7 @@
 #include <sys/poll.h>
 #include <sys/signal.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include "generated/test_protocol_v1-server.hpp"
 #include "generated/test_protocol_v1-client.hpp"
